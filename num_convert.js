82a9a2e227e93af13de80462e8e3b4b11c2a708e
Mani Amoozadeh <maniam@ucdavis.edu>
2019-12-31 17:20:18 -0800

    adding negative num presentation

--- num_convert.js
+++ num_convert.js
@@ -17,7 +17,7 @@ function convert(b) {
 		
 		num_dec = parseInt(a, 2);
 		
-		if (isNaN(num_dec) || num_dec > 4294967295) {
+		if (isNaN(num_dec) || num_dec < 0) {
 			document.getElementById("not_valid_bin").innerHTML = "<b>Invalid format!</b>";
 	        document.getElementById("oct").value = "";
 	        document.getElementById("dec").value = "";
@@ -42,7 +42,7 @@ function convert(b) {
 		
 		num_dec = parseInt(a, 8);
 		
-		if (isNaN(num_dec)) {
+		if (isNaN(num_dec) || num_dec < 0) {
 			document.getElementById("not_valid_oct").innerHTML = "<b>Invalid format!</b>";
 	        document.getElementById("bin").value = "";
 	        document.getElementById("dec").value = "";
@@ -67,7 +67,7 @@ function convert(b) {
 		
 		num_dec = parseInt(a, 10);
 		
-		if (isNaN(num_dec)) {
+		if (isNaN(num_dec) || num_dec < 0) {
 			document.getElementById("not_valid_dec").innerHTML = "<b>Invalid format!</b>";
 	        document.getElementById("bin").value = "";
 	        document.getElementById("oct").value = "";
@@ -92,7 +92,7 @@ function convert(b) {
 		
 		num_dec = parseInt(a, 16);
 		
-		if (isNaN(num_dec)) {
+		if (isNaN(num_dec) || num_dec < 0) {
 			document.getElementById("not_valid_hex").innerHTML = "<b>Invalid format!</b>";
 	        document.getElementById("bin").value = "";
 	        document.getElementById("oct").value = "";
@@ -110,6 +110,43 @@ function convert(b) {
 	}		
 }
 
+function convert_neg() {
+    var a = document.getElementById("neg_dec").value;
+    a = a.replace(/^\s+|\s+$/g, "");
+		
+	// remove all spaces
+	a = a.replace(/ /g,'');
+		
+	num_dec = parseInt(a, 10);
+		
+	if (isNaN(num_dec) || num_dec < 0) {
+		document.getElementById("neg_not_valid_dec").innerHTML = "<b>Invalid number!</b>";
+	    document.getElementById("bin_1").value = "";
+	    document.getElementById("bin_2").value = "";
+	    document.getElementById("bin_sign_mag").value = "";
+        return;
+	}
+	
+	if(num_dec > 2147483647) {
+		document.getElementById("neg_not_valid_dec").innerHTML = "<b>Number cannot excced 2147483647</b>";
+	    document.getElementById("bin_1").value = "";
+	    document.getElementById("bin_2").value = "";
+	    document.getElementById("bin_sign_mag").value = "";
+        return;
+	}
+	
+	document.getElementById("neg_not_valid_dec").innerHTML = "";
+			
+	document.getElementById("bin_1").innerHTML = binWithSeperators((~num_dec >>> 0).toString(2));
+	
+	var twos = (~num_dec + 1 >>> 0).toString(2);
+	document.getElementById("bin_2").innerHTML = binWithSeperators(FormatNumberLength(twos,32));
+	
+	var mag = FormatNumberLength(num_dec.toString(2), 32);
+	sign_mag = "<span style='color: red;'>1</span>" + mag.slice(1);
+	document.getElementById("bin_sign_mag").innerHTML = binWithSeperators(sign_mag);	
+}
+
 function binWithSeperators(x) {
     return x.replace(/\B(?=(\d{4})+(?!\d))/g, " ");
 }
@@ -121,3 +158,13 @@ function octWithSeperators(x) {
 function decWithSeperators(x) {
     return x.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+function FormatNumberLength(num, length) {
+    var r = "" + num;
+    while (r.length < length) {
+        r = "0" + r;
+    }
+    return r;
+}
+
+window.onload = convert_neg;
